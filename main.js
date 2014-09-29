4f8deb29b3922d5aa289fd26dfb246818e9d4ee6
Simeg44 <Simeg44@gmail.com>
2014-09-29 12:33:06 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var Quote = function(text, author, rating) {
+	this.text = text;
+	this.author = author || "unknown";
+	this.rating = rating || 0;
+}
+
+var one = new Quote("Fiction is the truth inside the lie.", "Stephen King");
+var two = new Quote("The best and most beautiful things in the world cannot be seen or even touched-they must be felt with the heart.", "Helen Keller");
+var three = new Quote("We know what we are, but know not what we may be.", "William Shakespeare");
+var four = new Quote("I never wanted to be famous. I only wanted to be great.", "Ray Charles");
+var five = new Quote("Do not take life too seriously. You will never get out of it alive.", "Elbert Hubbard");
+var six = new Quote("People who think they know everything are great annoyances to those of us who do.", "Issac Asimov");
+var seven = new Quote("Procrastination is the art of keeping up with yesterday.", "Don Marquis");
+var eight = new Quote("Get your facts first, then you can distort them as they please.", "Mark Twain");
+var nine = new Quote("Always remember you are absolutely unique. Just like everyone else.", "Margaret Mead");
+
+$(document).on('ready', function() {
+  
+});
